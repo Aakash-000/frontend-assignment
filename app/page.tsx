c75a33ecab5223a66a484d3ad22112e3271ee528
Aakash <aakashdhakal2430@gmail.com>
2023-09-02 21:43:02 +0545

    Updated 2023-09-02

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
     <main className="relative">
     <QueryClientProvider client={queryClient}>
     <Hero/>
-    <Products searchTerm=""/>
+    <Products/>
     </QueryClientProvider>
     </main>
   )
